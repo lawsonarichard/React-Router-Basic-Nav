820ce3068f1deb01c30a19a39ee6be58733b07a0
Richard Lawson <lawson.a.richard@gmail.com>
2019-07-22 13:06:00 -0700

    MVP, links work!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+// Inside of your `App.js` file `import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom';
 
 const App = () => (
+  <Route>
   <div>
     <Navigation />
   </div>
+
+  {/* // Create Three Routes */}
+  <Route path="/" exact component={ Home }/>
+  <Route path="/about" component={ About }/>
+  <Route path="/contact" component={ Contact }/>
+  </Route>
 );
+
 
 export default App;
